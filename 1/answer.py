67522e373da51e2d5bd019f5bd74d86fc967dcbb
Jasper Smulders <jasper.smulders@group.one>
2023-12-07 11:16:45 +0100

    Project setup and day one

---
+++ 1/answer.py
@@ -0,0 +1,63 @@
+import os
+import re
+import sys
+
+input_file = sys.argv[1] 
+
+def extract(line): 
+    first_match = re.search(r'\d{1,}', line).group()[0]
+    last_match = re.search(r'\d{1,}', line[::-1]).group()[0]
+
+    return int(first_match + last_match)
+
+def part_one():
+    with open(os.path.join(os.path.dirname(__file__), input_file), 'r') as file:
+        lines = file.readlines()
+
+    total = 0
+    for line in lines:
+        total = total + extract(line)
+
+    print("The answer for part one is:")
+    print(total) 
+
+def part_two():
+    mapped_numbers = {
+        "one": "1",
+        "two": "2",
+        "three": "3",
+        "four": "4",
+        "five": "5",
+        "six": "6",
+        "seven": "7",
+        "eight": "8",
+        "nine": "9",
+        "zero": "0",
+    }
+
+    with open(os.path.join(os.path.dirname(__file__), input_file), 'r') as file:
+        lines = file.readlines()
+
+    total = 0
+    for line in lines:
+        linelen = len(line)
+        c = 0
+        newline = ""
+        while c < linelen:
+            for k in mapped_numbers:
+                if line[c].isdigit():
+                    newline = newline + line[c]
+                    break
+                if line.startswith(k, c):
+                    newline = newline + mapped_numbers[k]
+                    break
+            c += 1
+        total = total + extract(newline)
+
+    print("-----------------")
+    print("The answer for part two is:")
+    print(total)
+
+if __name__ == '__main__':
+    part_one()
+    part_two()
