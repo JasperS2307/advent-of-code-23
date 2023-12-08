3bde00f2bc7fc2f1d2ca1d0427fca6c63b51193d
Jasper Smulders <jasper.smulders@group.one>
2023-12-08 17:13:00 +0100

    Day 6

--- 6/answer.py
+++ 6/answer.py
@@ -1,5 +1,6 @@
 import sys
 import os 
+from pprint import pprint
 
 input_file = sys.argv[1] 
 
@@ -7,7 +8,6 @@ with open(os.path.join(os.path.dirname(__file__), input_file), 'r') as file:
     lines = file.readlines()
 
 def part_one():
-    time = 0
     time_list = lines[0].strip().split(":")[1].split(" ")
     distance_list = lines[1].strip().split(":")[1].split(" ")
 
@@ -17,7 +17,32 @@ def part_one():
     print(time_list)
     print(distance_list)
 
-    print(f"The answer for part one is: {time}")
+    races = []
+    for i, t in enumerate(time_list):
+        races.append({
+            "time": int(time_list[i]),
+            "distance": int(distance_list[i])
+        })
+
+    possibilities_sum = 1
+    for race in races:
+        hold_ms = 0
+        race_time = race["time"]
+        race_possibilities = 0
+        while hold_ms < race_time:
+            time_left = race_time - hold_ms
+            distance_travelled = hold_ms * time_left
+            if distance_travelled > race["distance"]:
+                race_possibilities += 1
+
+            print(f"Holding it {hold_ms}ms, {time_left}ms left, {distance_travelled} distance travelled")
+            hold_ms += 1
+        
+        possibilities_sum = possibilities_sum * race_possibilities
+        print(f"Next race! Possibilities: {race_possibilities}")
+
+    pprint(races)
+    print(f"The answer for part one is: {possibilities_sum}")
 
 def part_two():
     score = 0
