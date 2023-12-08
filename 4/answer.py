7f7e91b48fb697491b0b5eb0ad8e3250d8f6a7a5
Jasper Smulders <jasper.smulders@group.one>
2023-12-08 16:42:36 +0100

    More days

--- 4/answer.py
+++ 4/answer.py
@@ -33,32 +33,34 @@ def part_two():
     extra_cards = {}
 
     for card_set, line in enumerate(lines, start=1):
-        card_count += 1
-        line = line.strip()
-        game, cards = line.split(":")
-        game = re.search(r'\d+', game).group()[0]
-        winning_cards_set, my_cards_set = cards.split("|")
-        winning_cards = winning_cards_set.split(" ")
-        my_cards = my_cards_set.split(" ")
-        
-        winning_cards_count = 0
-        for my_card in my_cards:
-            if my_card.isdigit() and my_card in winning_cards:
-                winning_cards_count += 1
-                if str(game + winning_cards_count) not in extra_cards:
-                    extra_cards = {game + winning_cards_count: 0}
-                
-                extra_cards[game + winning_cards_count] += 1
-        
-        
-        print(f"Game {game} has {winning_cards_count} winning cards")
-    
-    print(extra_cards)
+        if card_set not in extra_cards:
+            extra_cards.update({card_set: 1})
 
+        i = 1
+        while i <= extra_cards[card_set]:
+            card_count += 1
+            line = line.strip()
+            game, cards = line.split(":")
+            game = int(re.search(r'\d+', game).group()[0])
+            winning_cards_set, my_cards_set = cards.split("|")
+            winning_cards = winning_cards_set.split(" ")
+            my_cards = my_cards_set.split(" ")
+            
+            winning_cards_count = 0
+            for my_card in my_cards:
+                if my_card.isdigit() and my_card in winning_cards:
+                    winning_cards_count += 1
+                    if card_set + winning_cards_count not in extra_cards:
+                        extra_cards.update({card_set + winning_cards_count: 1})
+                    
+                    extra_cards[card_set + winning_cards_count] += 1
+
+            
+            i += 1
+    
     print("-----------------")
-    print("The answer for part two is:")
-    print("")
+    print(f"The answer for part two is: {card_count}")
 
 if __name__ == '__main__':
-    # part_one()
+    part_one()
     part_two()
--- 6/answer.py
+++ 6/answer.py
@@ -7,8 +7,17 @@ with open(os.path.join(os.path.dirname(__file__), input_file), 'r') as file:
     lines = file.readlines()
 
 def part_one():
-    score = 0
-    print(f"The answer for part one is: {score}")
+    time = 0
+    time_list = lines[0].strip().split(":")[1].split(" ")
+    distance_list = lines[1].strip().split(":")[1].split(" ")
+
+    time_list = [t for t in time_list if t.isdigit()]
+    distance_list = [t for t in distance_list if t.isdigit()]
+
+    print(time_list)
+    print(distance_list)
+
+    print(f"The answer for part one is: {time}")
 
 def part_two():
     score = 0
